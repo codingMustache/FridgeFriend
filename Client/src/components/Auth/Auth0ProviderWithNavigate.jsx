0d1e64c42903b8e8fa7e81a0b2904ed8384b9c77
eekbk <eekeric@gmail.com>
2023-04-01 13:36:51 -0500

    add: login Button and Auth provider

--- Client/src/components/Auth/Auth0ProviderWithNavigate.jsx
+++ Client/src/components/Auth/Auth0ProviderWithNavigate.jsx
@@ -1,29 +1,29 @@
 import { Auth0Provider } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Auth0ProviderWithNavigate = ({ children }) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const { DOMAIN } = process.env;
-  const { CLIENT_ID } = process.env;
-  const { CALLBACK_URL } = process.env;
+  const { VITE_DOMAIN, VITE_CLIENT_ID } = import.meta.env;
+  // const { CLIENT_ID } = process.env;
+  // const { CALLBACK_URL } = process.env;
 
-  const onRedirectCallback = appState => {
-    navigate(appState?.returnTo || window.location.pathname);
-  }
+  // const onRedirectCallback = appState => {
+  //   navigate(appState?.returnTo || window.location.pathname);
+  // }
 
-  if (!(DOMAIN && CLIENT_ID && CALLBACK_URL)) {
-    return null;
-  }
+  // if (!(DOMAIN && CLIENT_ID && CALLBACK_URL)) {
+  //   return null;
+  // }
 
   return (
     <Auth0Provider
-      domain={DOMAIN}
-      clientId={CLIENT_ID}
+      domain={VITE_DOMAIN}
+      clientId={VITE_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: CALLBACK_URL,
+        redirect_uri: window.location.origin,
       }}
-      onRedirectCallback={onRedirectCallback}
+      // onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
