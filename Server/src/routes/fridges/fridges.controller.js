1708c4c9b21d18a03651e7d901e5fe7e8e5c0936
codingMustache <codingMustache@icloud.com>
2023-04-01 19:56:43 -0500

    fridge api get by id and get all

--- Server/src/routes/fridges/fridges.controller.js
+++ Server/src/routes/fridges/fridges.controller.js
@@ -2,15 +2,22 @@ const { Fridge } = require('../../models/index')
 const axios = require('axios')
 const MapBox_Token = process.env.MapBox_Token;
 /*
-export const getAllFridgesGeoCode = () => {}
 export const getFridgeAddress = () => {}
 export const getFridgeGeoCode = () => {}
 export const getFridgesImagesByID = () => {}
 */
+const getAllFridgesGeoCode = async (req, res) => {
+  Fridge.find({}).select('location')
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => error.log(err))
+}
+
 const addFridge = async (req, res) => {
   const fridgeAddress = req.body.address
   axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${fridgeAddress}.json?language=en&limit=5&proximity=-121.90662,37.42827&country=US&access_token=${MapBox_Token}`)
-    .then(({data}) => {
+    .then(({ data }) => {
       const geocode = data.features[0].center
       const lon = geocode[0]
       const lat = geocode[1]
@@ -29,7 +36,16 @@ const addFridge = async (req, res) => {
   res.sendStatus(200)
 }
 
+const getFridgeInfoByID = (req, res) => {
+  console.log(req.params.id)
+  Fridge.findOne({"_id": req.params.id})
+  .then(data => res.send(data))
+  .catch(err => error.log(err))
+}
+
 module.exports = {
-  addFridge
+  addFridge,
+  getAllFridgesGeoCode,
+  getFridgeInfoByID
 }
 
