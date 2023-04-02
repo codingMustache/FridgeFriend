bb6e0f0d8c625c4d3f1dfa2d5857bc57f6724276
codingMustache <codingMustache@icloud.com>
2023-04-01 19:08:30 -0500

    clean up

--- Server/src/routes/fridges/fridges.controller.js
+++ Server/src/routes/fridges/fridges.controller.js
@@ -1,5 +1,6 @@
-const Fridge = require('../../models/index')
+const { Fridge } = require('../../models/index')
 const axios = require('axios')
+const MapBox_Token = process.env.MapBox_Token;
 /*
 export const getAllFridgesGeoCode = () => {}
 export const getFridgeAddress = () => {}
@@ -7,14 +8,25 @@ export const getFridgeGeoCode = () => {}
 export const getFridgesImagesByID = () => {}
 */
 const addFridge = async (req, res) => {
-  console.log(req)
-  //const fridgeAdress = fridgeToAdd.address
-  //console.log(fridgeAdress)
-  // axios(
-  //   `https://api.mapbox.com/geocoding/v5/mapbox.places/${formattedLocation}.json?language=en&limit=5&proximity=-121.90662,37.42827&country=US&access_token=${mapToken}`
-  // )
-  //Fridge.create(data)
-  res.send()
+  const fridgeAddress = req.body.address
+  axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${fridgeAddress}.json?language=en&limit=5&proximity=-121.90662,37.42827&country=US&access_token=${MapBox_Token}`)
+    .then(({data}) => {
+      const geocode = data.features[0].center
+      const lon = geocode[0]
+      const lat = geocode[1]
+      const fridgeDbObj = {
+        location: {
+          lat,
+          lon,
+          address: {
+            streetAddress: fridgeAddress,
+          }
+        },
+      }
+      Fridge.create(fridgeDbObj)
+    })
+    .catch(err => error.log(err))
+  res.sendStatus(200)
 }
 
 module.exports = {
